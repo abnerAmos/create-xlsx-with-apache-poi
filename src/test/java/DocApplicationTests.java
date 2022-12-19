89afa629fdaa2c5a198cacc130dc3fcc833f623c
abnerAmos <abner.fsouza1@gmail.com>
2022-12-19 15:18:52 -0300

    Criando Arquivo Excel e testando

--- src/test/java/DocApplicationTests.java
+++ src/test/java/DocApplicationTests.java
@@ -1,5 +1,3 @@
-package com.example.excel;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
--- src/test/java/com/example/excel/TestCreateExcel.java
+++ src/test/java/com/example/excel/TestCreateExcel.java
@@ -1,2 +1,28 @@
-package com.example.excel;public class TestCreateExcel {
+package com.example.excel;
+
+import com.example.excel.model.Client;
+import com.example.excel.relatoryXLSX.CreateExcel;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestCreateExcel {
+
+    private final List<Client> clients = new ArrayList<>();
+
+    @BeforeEach
+    public void setup() {
+        clients.add(new Client(1, "Abner Amos de Souza", "abner@email.com", "11973851774"));
+        clients.add(new Client(2, "Juliana Menezes Silva", "juliana@email.com", "11932026167"));
+        clients.add(new Client(3, "Anna Beatriz de Jesus", "anna@email.com", "11981756810"));
+    }
+
+    @Test
+    void createFile() throws IOException {
+        var createFileExcel = new CreateExcel();
+        createFileExcel.createFile("clients.xlsx", clients);
+    }
 }
