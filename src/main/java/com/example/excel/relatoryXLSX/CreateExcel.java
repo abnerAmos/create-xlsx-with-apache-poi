59c467aefb0b7711f66ccdeb9703aa23eb5ebee8
abnerAmos <abner.fsouza1@gmail.com>
2022-12-20 09:33:50 -0300

    Att

--- src/main/java/com/example/excel/relatoryXLSX/CreateExcel.java
+++ src/main/java/com/example/excel/relatoryXLSX/CreateExcel.java
@@ -29,12 +29,12 @@ public class CreateExcel {
             addHeader(spreadsheet, countNumberLine++); // recebe a Planilha e Numero de Linhas
 
             for (Client e : client) {   // For para iteração dos dados do cliente - Uma linha para cada cliente
-                XSSFRow linha = spreadsheet.createRow(countNumberLine++);   //
+                XSSFRow linha = spreadsheet.createRow(countNumberLine++);
                 addCell(linha, 0, e.getId());
                 addCell(linha, 1, e.getName());
                 addCell(linha, 2, e.getEmail());
                 addCell(linha, 3, e.getContact());
-        }
+            }
             workbook.write(outputStream);   // Faz a gravação dos dados na Tabela
         } catch (FileNotFoundException e) {
             log.error("Arquivo não encontrado: {}", fileName);  // Erro caso não encontre o arquivo
@@ -44,21 +44,21 @@ public class CreateExcel {
         log.info("Arquivo gerado com sucesso!");
     }
 
-        private void addHeader(XSSFSheet spreadsheet, int countNumberLine) { // recebe a Planilha e Numero de Linhas
-            XSSFRow line = spreadsheet.createRow(countNumberLine); // Cria Colunas na Tabela
-            addCell(line, 0, "Id");
-            addCell(line, 1, "Name");
-            addCell(line, 2, "Email");
-            addCell(line, 3, "Contact");  // Adiciona os "Titulos" em cada coluna
-        }
-
-        private void addCell(Row line, int column, String valor) { // Métodos que adiciona campos com valor String
-            Cell cell = line.createCell(column);    // Informa qual coluna esta posicionado
-            cell.setCellValue(valor);               // Informa o valor da coluna/linha
+    private void addHeader(XSSFSheet spreadsheet, int countNumberLine) { // recebe a Planilha e Numero de Linhas
+        XSSFRow line = spreadsheet.createRow(countNumberLine); // Cria Colunas na Tabela
+        addCell(line, 0, "Id");
+        addCell(line, 1, "Name");
+        addCell(line, 2, "Email");
+        addCell(line, 3, "Contact");  // Adiciona os "Titulos" em cada coluna
     }
 
-        private void addCell(Row line, int column, int valor) { // Métodos que adiciona campo com valor String
-            Cell cell = line.createCell(column);    // Informa qual coluna esta posicionado
-            cell.setCellValue(valor);               // Informa o valor da coluna/linha
-        }
+    private void addCell(Row line, int column, String valor) { // Método que adiciona campos com valor String
+        Cell cell = line.createCell(column);    // Informa qual coluna esta posicionado
+        cell.setCellValue(valor);               // Informa o valor da coluna/linha
+    }
+
+    private void addCell(Row line, int column, int valor) { // Método que adiciona campo com valor Integer
+        Cell cell = line.createCell(column);    // Informa qual coluna esta posicionado
+        cell.setCellValue(valor);               // Informa o valor da coluna/linha
+    }
 }
